901e4d0496856fd968f996a7de6884bc740c8390
burakotlu <51903199+burakotlu@users.noreply.github.com>
2020-01-06 11:53:39 +0300

    Add files via upload

--- cache/cache.cpp
+++ cache/cache.cpp
@@ -2,7 +2,7 @@
 
 
 mem_type mem[MEM_SIZE];
-cache_mem_type cache_mem [INDEX_SIZE][OFFSET_SIZE]; //1 Kbyte*4 cache memory creation
+cache_mem_type cache_mem [INDEX_SIZE][OFFSET_SIZE]; //1 Kbyte*16 cache memory creation
 valid_type valid[INDEX_SIZE]; // valid array creation
 tag_type tag[INDEX_SIZE]; // tag array creation
 
@@ -15,10 +15,13 @@ add_type cache (add_type address) {
 	value=0;
 	index= (ind_type) (address >> INDEX_SHAMT) & INDEX_MASK ;
 	tag_add= (tag_type) (address >> TAG_SHAMT) & TAG_MASK ;
-	offset= (offset_type) address & TAG_MASK ;
-
+	offset= (offset_type) address & OFFSET_MASK ;
+	offset_type off_inc=(offset>>2)<<2;
+	//printf("index: %d\n",(uint32_t)index);
 	if (valid[index]==N){
 		//printf("miss.."); // case "cache miss"
+		//printf("fetching block from memory..");
+		//printf("f_a: %d\n",(uint32_t)address);
 		fetch(tag_add,index);// fetch corresponding block from memory
 		valid[index]=Y; //set valid to Yes
 		tag[index]=tag_add;
@@ -26,9 +29,10 @@ add_type cache (add_type address) {
 	else if (valid[index]==Y){ // valid is Yes
 		if(tag[index]==tag_add){ //tag is the same --> case "cache hit"
 			//printf("hit..fetching block from cache..");
-			for (int i=0;i<OFFSET_SIZE;i++){
+			//printf("f_a: %d\n",(uint32_t)address);
+			for (int i=0;i<4;i++){
 				value=value<<8;
-				value=value|cache_mem[index][i]; // read value from cache
+				value=value|cache_mem[index][i+off_inc]; // read value from cache
 				//printf("value: %x\n",(uint32_t)value);
 			}
 
