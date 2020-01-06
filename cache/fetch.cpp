db7544d7f59fa9bcded660cefd12f43a004cde3b
burakotlu <51903199+burakotlu@users.noreply.github.com>
2020-01-06 11:56:33 +0300

    Add files via upload

--- cache/fetch.cpp
+++ cache/fetch.cpp
@@ -7,13 +7,14 @@ extern offset_type offset;
 void fetch(tag_type tag_a,ind_type ind_a){
 	add_type f_a=0;
 	value=0;
-	f_a =((((f_a | (add_type)tag_a)<<(INDEX_LENGTH))| (add_type)ind_a)<<OFFSET_LENGTH);
-	//printf("fetching block from memory..");
-	//printf("f_a: %d\n",(uint32_t)f_a);
+	offset_type off_i=(offset>>2);
+	f_a =((((add_type)tag_a)<<INDEX_LENGTH)| (add_type)ind_a)<<OFFSET_LENGTH;
 	for (int i=0;i<OFFSET_SIZE;i++){
-		value=value<<8;
 		cache_mem[ind_a][i]=mem[f_a+i];
-		value=value|mem[f_a+i];
+		if(off_i==(i>>2)){
+			value=value<<8;
+			value=value|mem[f_a+i];
+		}
 		//printf("value: %x\n",(uint32_t)value);
 //	if (i==offset)
 //		value=cache_mem[ind_a][i];
